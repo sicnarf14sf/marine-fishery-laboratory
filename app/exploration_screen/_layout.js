17f6a5df670f1b6f76d0aca9e49dd32efc7aea68
sicnarf14sf <84180843+sicnarf14sf@users.noreply.github.com>
2023-12-09 13:34:44 +0800

    working 3D loading function

--- app/exploration_screen/_layout.js
+++ app/exploration_screen/_layout.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
 import { Stack } from 'expo-router'
 
 
@@ -14,16 +13,10 @@ const StackLayout = () => {
     <Stack>
         <Stack.Screen name="exploration" options={{headerShown: true, headerTitle: '', presentation: 'modal'}}/>
         <Stack.Screen name="show_fish_2d" options={{headerShown: false, presentation: 'modal'}} />
-        <Stack.Screen name="show_fish_3d" options={{headerShown: false, presentation: 'modal'}} />
+        <Stack.Screen name="show_fish_3d" options={{headerShown: true}} />
     </Stack>
   )
 }
 
-const styles = StyleSheet.create({
-  header: {
-    height: 0,
-  },
-
-})
 
 export default StackLayout
--- app/exploration_screen/show_fish_3d.js
+++ app/exploration_screen/show_fish_3d.js
@@ -1,26 +1,46 @@
-// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { Canvas, useFrame } from '@react-three/fiber';
+import React, { useState, useRef, useMemo, Suspense, useLayoutEffect }  from 'react';
 import { useLoader } from '@react-three/fiber'
-import React, { useState, useRef, useMemo, Suspense }  from 'react';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { TextureLoader } from 'expo-three';
 
 function Fish() {
-  const fish = useLoader(OBJLoader, require('../../public/Pompano.obj'))
+  const [base] = useLoader(TextureLoader, [
+    require('../../assets/parrotFish/parrotFish.png'),
+  ]);
 
-  return <mesh>
-    <primitive object={fish} scale={10}/>
-  </mesh>  
+  const material = useLoader(MTLLoader, require('../../assets/parrotFish/parrotFish.mtl'));
+
+  const obj = useLoader(
+    OBJLoader,
+    require('../../assets/parrotFish/parrotFish.obj'),
+    (loader) => {
+      material.preload();
+      loader.setMaterials(material);
+    }
+  );
+
+  useLayoutEffect(() => {
+    obj.traverse((child) => {
+      if (child instanceof THREE.Mesh) {
+        child.material.map = base;
+      }
+    });
+  }, [obj]);
+
+
+  return <primitive object={obj} scale={0.015} />
 }
 
 export default function show_fish_3d() {
   return (
     <Canvas>
       <ambientLight />
-      <pointLight position={[10,10,10]} intensity={1000} />
+      <pointLight position={[10,10,10]} />
       <Suspense fallback={null}>
         <Fish />
       </Suspense> 
     </Canvas>
   );
 }
-
